dd3634d52c92a36ff6e011b04a3ee8f1899c07e3
Максим Водянов <max.post@bk.ru>
2020-03-01 17:57:48 +0300

    Минимальная рабочая реализация Virtual DOM: basic render

--- samples/App.jsx
+++ samples/App.jsx
@@ -1,5 +1,5 @@
 import OwnReact from "../src";
 
-const App = <h1 prop1="prop value">Hello, World!</h1>;
+const App = <h1 prop1="prop1 value" prop2="prop2 value">Hello, World! <br/> sksk</h1>;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 class OwnReact {
-  static createElement(...args) {
-    return [...args];
-  }
+    static createElement(type, props, ...children) {
+        return {
+            type: type,
+            props: {
+                ...props,
+                children: [...children]
+            },
+        };
+    }
+
+    static render(element, parentDom) {
+        const isTextElement = typeof element === 'string';
+        const {type, props} = element;
+
+        const dom = isTextElement
+            ? document.createTextNode(element)
+            : document.createElement(type);
+        if (!isTextElement) {
+            const childElements = props.children || [];
+            childElements.forEach(childElement => this.render(childElement, dom));
+        }
+        parentDom.appendChild(dom);
+    }
 }
 
 export default OwnReact;
