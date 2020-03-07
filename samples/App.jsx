2a0e5deb4e72280bd12d293da48a3c3ed78f61fa
Максим Водянов <max.post@bk.ru>
2020-03-07 10:53:29 +0300

    Минимальная рабочая реализация Virtual DOM: instance

--- samples/App.jsx
+++ samples/App.jsx
@@ -1,16 +1,41 @@
 import OwnReact from "../src";
 
-const alphabet = ["a", "b", "c"];
+const alphabet = ["a", "b"];
 
 const List = children => <ul>{children}</ul>;
 const ListItem = children => <li>{children}</li>;
 
+// const App = (
+//   <List>
+//     {alphabet.map(el => (
+//       <ListItem>{el}</ListItem>
+//     ))}
+//   </List>
+// );
+
 const App = (
-  <List>
-    {alphabet.map(el => (
-      <ListItem>{el}</ListItem>
-    ))}
-  </List>
+  <ul>
+    <li>a</li>
+    <li>b</li>
+  </ul>
 );
+
+const App1 = (
+  <div><ul>
+    asdf
+     {alphabet.map(el => <li>{el}</li>)}
+  </ul></div>
+);
+
+//samples/index.js
+const root = document.getElementById("root");
+function tick() {
+  OwnReact.render(App, root);
+}
+// tick();
+// setInterval(tick, 1000);
+// eslint-disable-next-line react/no-deprecated
+OwnReact.render(App, root);
+OwnReact.render(App1, root);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 class OwnReact {
+  constructor() {
+    this.rootInstance = null;
+  }
+
   static createElement(type, props, ...children) {
     return {
       type,
@@ -9,27 +13,85 @@ class OwnReact {
     };
   }
 
-  static render(element, parentDom) {
+  static render(element, container) {
+    const prevInstance = this.rootInstance;
+    const nextInstance = this.reconcile(container, prevInstance, element);
+    this.rootInstance = nextInstance;
+    console.info("ROOT INSTANCE", this.rootInstance);
+    // this.rootInstance = this.reconcile(container, this.rootInstance(), element);
+  }
+
+  static reconcile(parentDom, instance, element) {
+    if (instance == null) {
+      const newInstance = this.instantiate(element);
+      parentDom.appendChild(newInstance.dom);
+      return newInstance;
+    }
+    else if (instance.element.type === element.type) {
+      // Обновляем инстанс
+      // updateDomProperties(instance.dom, instance.element.props, element.props);
+      instance.childInstances = this.reconcileChildren(instance, element);
+      instance.element = element;
+      return instance;
+    }
+    else {
+      // Заменяем инстанс
+      const newInstance = this.instantiate(element);
+      parentDom.replaceChild(newInstance.dom, instance.dom);
+      return newInstance;
+    }
+  }
+
+  static reconcileChildren(instance, element) {
+    const dom = instance.dom;
+    const childInstances = instance.childInstances;
+    const nextChildElements = element.props.children || [];
+    const newChildInstances = [];
+    const count = Math.max(childInstances.length, nextChildElements.length);
+    for (let i = 0; i < count; i++) {
+      const childInstance = childInstances[i];
+      const childElement = nextChildElements[i];
+      const newChildInstance = this.reconcile(dom, childInstance, childElement);
+      newChildInstances.push(newChildInstance);
+    }
+    return newChildInstances;
+  }
+
+  static instantiate(element) {
+
     const { type, props } = element;
     if (typeof element === "string") {
       const textElement = this.createElement("TEXT_ELEMENT", {
         nodeValue: element
       });
-      this.render(textElement, parentDom);
+      return this.instantiate(textElement);
     } else if (element instanceof Array) {
-      element.map(item => this.render(item, parentDom));
+       return element.map(item => this.instantiate(item));
     } else if (type instanceof Function) {
-      this.render(type(props.children), parentDom);
+      this.instantiate(type(props.children));
     } else if (type instanceof Object) {
-      this.render(type, parentDom);
+      this.instantiate(type);
     } else {
       const isTextElement = type === "TEXT_ELEMENT";
       const dom = isTextElement
         ? document.createTextNode(props.nodeValue)
         : document.createElement(type);
       const childElements = props.children || [];
-      childElements.forEach(childElement => this.render(childElement, dom));
-      parentDom.appendChild(dom);
+      const childInstances = childElements.map(child => this.instantiate(child));
+      const childDoms = childInstances.reduce((doms, childInstance) => {
+        if (childInstance instanceof Array) {
+          childInstance.map(instance => doms.push(instance.dom));
+        } else {
+          doms.push(childInstance.dom);
+        }
+        return doms
+      }, []);
+      console.info(childDoms);
+      childDoms.forEach(childDom => {
+        return dom.appendChild(childDom)
+      });
+      const instance = { dom, element, childInstances };
+      return instance;
     }
   }
 }
